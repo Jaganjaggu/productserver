8134bff2f51c0b43a1776b0ae1a4036001fcd25a
Jaganjaggu <jaganvofficial@gmail.com>
2024-02-24 17:00:35 +0530

    MONGODB atlas added in .env file

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
-// const connectionString = process.env.DATABASE
-mongoose.connect("mongodb://127.0.0.1:27017/productsApp").then(()=>{
+const connectionString = process.env.DATABASE
+
+// using local mongodb
+// mongoose.connect("mongodb://127.0.0.1:27017/productsApp").then(()=>{
+//     console.log("Mongoose connection Successfully connected with Product Server");
+// }).catch((err)=>{
+//     console.log(`Mongodb connection failed!! Error: ${err}`);
+// })
+
+mongoose.connect(connectionString).then(()=>{
     console.log("Mongoose connection Successfully connected with Product Server");
 }).catch((err)=>{
     console.log(`Mongodb connection failed!! Error: ${err}`);
